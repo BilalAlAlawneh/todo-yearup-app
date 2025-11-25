c5631d0bb71cb54c5c7be4081c391e4262f1cfd2
Nik <nikkusaka@gmail.com>
2025-11-25 09:43:10 -0800

    First Commit

---
+++ src/main/java/com/companyname/Main.java
@@ -0,0 +1,11 @@
+package com.companyname;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.printf("Todo App");
+
+    }
+}
